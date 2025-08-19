6900132934ae34937f503d9b634f4e52e6299f2a
moritzgvt <hello@moritzgut.de>
2025-08-19 13:54:19 +0200

    Prepare for release 0.9.98

--- gdy-modular-content.php
+++ gdy-modular-content.php
@@ -5,14 +5,14 @@ Description: Create and edit modular content from the frontend of your site.
 Text Domain: gdy-modular-content
 Domain Path: /languages/
 License: GPLv2
-Version: 0.9.97
+Version: 0.9.98
 */
 
 
 
 	/************************************* VERSION ***********************************/
 
-	define( 'GDYMC_PLUGIN_VERSION', '0.9.97' );
+	define( 'GDYMC_PLUGIN_VERSION', '0.9.98' );
 
 
 	/************************************* PLUGIN BASE ***********************************/
