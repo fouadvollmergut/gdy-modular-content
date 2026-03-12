049028915e5b06e1fa13e88c31af04db43052fcf
moritzgvt <hello@moritzgut.de>
2026-03-12 17:25:28 +0000

    0.10.2 Hotfix - Add missing `status` property of gdymc_module blueprint object to comparision, when addding module in gdymc_area

--- includes/area.php
+++ includes/area.php
@@ -76,7 +76,7 @@
             $gdymc_module = new GDYMC_MODULE( $id, $gdymc_object_id, $gdymc_object_type );
 
             // If module blueprint ist inactive, skip it
-            if ( gdymc_get_module($gdymc_module->type) === 'INACTIVE' ) continue;
+            if ( gdymc_get_module($gdymc_module->type)->status === 'INACTIVE' ) continue;
 
             // If module is visible
             if( $gdymc_module->is_visible() OR gdymc_logged() ):
